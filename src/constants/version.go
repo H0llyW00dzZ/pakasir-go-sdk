d2a619f45431b88f64cf1fd97beeac184bd0415c
H0llyW00dzZ <H0llyW00dzZ@pm.me>
2026-04-07 14:03:44 +0700

    Bump Version
    - [+] chore(version): bump SDKVersion to 0.2.0

--- src/constants/version.go
+++ src/constants/version.go
@@ -23,7 +23,7 @@ const (
 
 	// SDKVersion is the current version of this SDK.
 	// This should be updated with each release.
-	SDKVersion = "0.1.1"
+	SDKVersion = "0.2.0"
 
 	// SDKRepository is the GitHub repository URL for this SDK.
 	SDKRepository = "https://github.com/H0llyW00dzZ/pakasir-go-sdk"
